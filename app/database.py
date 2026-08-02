adf27e0bb15bce8e70cd6d149388378fbfec9656
Dantesos33 <dantesos33@gmail.com>
2026-08-03 02:25:51 +0500

    made some change

--- app/database.py
+++ app/database.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from app.config import settings
+# Pass SSL parameters directly inside the connect_args dictionary
+engine = create_engine(
+    clean_url, 
+    pool_pre_ping=True,
+    connect_args={"ssl": {"ssl_mode": "REQUIRED"}}
+)
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
